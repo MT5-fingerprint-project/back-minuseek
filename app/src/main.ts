15cfdcef2780d7e4bf064142b1a72339b5ff0cff
Doud75 <adrienquimbre@gmail.com>
2026-06-02 09:16:42 +0200

    refacto : add /api before each url except docs

--- app/src/main.ts
+++ app/src/main.ts
@@ -5,13 +5,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Minuseek API')
     .setVersion('1.0')
     .build();
-  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, config));
+  app.setGlobalPrefix('api', { exclude: ['/docs'] });
+  SwaggerModule.setup('docs', app, SwaggerModule.createDocument(app, config));
 
   await app.listen(process.env.PORT ?? 3000);
 }
