9e09e95eefca73d71ff68eb30efd8c8b00977ffd
Adrien Quimbre <80105952+Doud75@users.noreply.github.com>
2026-06-03 12:04:57 +0200

    feat: cors security and docker network (#15)

--- app/src/main.ts
+++ app/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: process.env.ORIGIN, credentials: true });
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
